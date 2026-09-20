beccc297aad2fb07bdf1179984a93cf86790c6aa
mewmery <oiinkgames@gmail.com>
2026-09-20 19:27:45 +1000

    Remove unused image columns from tables

--- backend/storage/migrations/2025-05-07.09.16-remove-unused.sql
+++ backend/storage/migrations/2025-05-07.09.16-remove-unused.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `mythicaldash_eggs` DROP COLUMN IF EXISTS `image`;
-ALTER TABLE `mythicaldash_locations` DROP COLUMN IF EXISTS `image`;
+ALTER TABLE `mythicaldash_eggs` DROP COLUMN `image`;
+ALTER TABLE `mythicaldash_locations` DROP COLUMN `image`;
